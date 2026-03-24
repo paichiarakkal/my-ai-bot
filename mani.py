f5bbc7dd7b5080305199e39aab7b32d778fe83d0
paichiarakkal <paichiarakkal@gmail.com>
2026-03-24 17:36:12 +0400

    Update model to use latest version

--- mani.py
+++ mani.py
@@ -6,7 +6,7 @@ TELEGRAM_BOT_TOKEN = '8638662433:AAGKc6Uo-X06W6i2Sdt5Ul1bIpv_FoXhVjQ'
 GEMINI_API_KEY = 'AIzaSyC9CUkynm2xlmBDiObx6xkfFi0Dm9vqg_4'
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
