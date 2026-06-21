8ac579b437f1fc69d2a2aef901425984f162627f
paichiarakkal <paichiarakkal@gmail.com>
2026-06-21 15:26:08 +0400

    Add HTTP headers to YouTube download options

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ if st.button("Download Video"):
         ydl_opts = {
             'format': 'best',
             'outtmpl': '%(title)s.%(ext)s',
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+            },
+            'nocheckcertificate': True,
+            'quiet': True,
         }
         
         try:
